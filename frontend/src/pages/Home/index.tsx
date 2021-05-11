78d70cebff82e32a29253a7326c54ec9254a3de4
Pedro Nery <pedrohnery@gmail.com>
2021-05-11 11:44:09 -0300

    Alter

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
+import NavBar from "components/NavBar/index";
+import Footer from "components/Footer/index";
 import { Link } from "react-router-dom";
 
 
